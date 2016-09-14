1d4a079f769769337210d7eea5b464c913131146
ekhaydarov <erkin.khaydarov@holidayextras.com>
2016-09-14 20:37:37 +0100

    init barebones

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='packagetest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='https://github.com/ekhaydarov/packagetest',
+      author='jeff',
+      author_email='example@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
